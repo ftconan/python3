cfb5d2439d54545e1a6452ca6b5054d76859c023
pinmingkenan <1508907793@qq.com>
2018-06-24 15:21:02 +0800

    '数字游戏优化，添加随机数字，添加三次错误机会限制'

--- number_game.py
+++ number_game.py
@@ -4,6 +4,7 @@
 @author: conan
 @date: 2018/6/24
 """
+import random
 
 
 def number_game():
@@ -14,18 +15,31 @@ def number_game():
     print('*'*8 + '开始数字游戏' + '*'*8)
 
     #  游戏竞猜循环
+    count = 0
+
+    secret = random.randint(0, 10)
     while True:
-        number = input('请输入一个我喜欢的数字:\n')
-        try:
-            number = int(number)
-            if number == 8:
-                print('恭喜您猜中啦!')
-                break
-            else:
-                print('猜错咯，继续努力!')
-        except Exception as e:
-            print(e)
-            print('请输入一个正确的数字!')
+        # 计数器，三次都没猜中，跳出循环
+        if count != 3:
+            number = input('请输入一个我喜欢的数字,只有三次机会哦!:\n')
+            try:
+                number = int(number)
+                if number == secret:
+                    print('恭喜您猜中啦!')
+                    break
+                else:
+                    if number > secret:
+                        print('猜错咯!数字大了一点点!')
+                    else:
+                        print('猜错咯!数字小了一丢丢!')
+            except Exception as e:
+                print(e)
+                print('请输入一个正确的数字!')
+        else:
+            print('三次都没猜中哦，GAME OVER!')
+            break
+
+        count += 1
 
     print('游戏结束咯!')
 
