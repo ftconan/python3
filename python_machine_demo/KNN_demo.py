fa181d10f2a06ef94773fc2e2f002ad0142146c9
magician <1508907793@qq.com>
2018-12-03 22:16:02 +0800

    numpy_description

--- python_machine_demo/KNN_demo.py
+++ python_machine_demo/KNN_demo.py
@@ -4,10 +4,12 @@
 @author: magician
 @date: 2018/11/14
 """
-pass
+import numpy as np
+import pandas as pd
 
 
 if __name__ == '__main__':
     # 读取鸢尾花的数据集,header参数来指定标题行,默认为0,如果没有标题,使用None
-    data = pd.read_csv(r'python_machine_demo/data_set/iris.csv', header=0)
-    pass
+    data = pd.read_csv(r'/home/magician/project/python3/python_machine_demo/data_set/iris.csv', header=0)
+    print(data)
+
