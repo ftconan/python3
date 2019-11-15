0c704f3882ccd3d9ef29d6b81fc55d51472392b1
ftconan <1508907793@qq.com>
2019-11-15 16:23:36 +0800

    update assert_demo

--- python_tricks/assert_demo.py
+++ python_tricks/assert_demo.py
@@ -7,7 +7,7 @@
 
 def apply_discount(product, discount):
     """
-    appy discount
+    apply discount
     :param product:
     :param discount:
     :return:
@@ -19,7 +19,36 @@ def apply_discount(product, discount):
     return price
 
 
+class AuthError(object):
+    """
+    AuthError
+    """
+    def __init__(self, error):
+        self.error = error
+
+
+def delete_product(product_id, user, store=None):
+    """
+    delete product
+    :param product_id:
+    :param user:
+    :param store:
+    :return:
+    """
+    if not user.is_admin():
+        raise AuthError('Must be admin to delete')
+    if not store.has_product(product_id):
+        raise ValueError('Unknown product id')
+
+    store.get_product(product_id).delete()
+
+    return True
+
+
 if __name__ == '__main__':
     shoes = {'name': 'Fancy Shoes', 'price': 14900}
     apply_discount(shoes, 0.25)
     apply_discount(shoes, 2.0)
+    apply_discount(prod, 2.0)
+    #  SyntaxWarning: assertion is always true
+    assert(1 == 2, 'This should fail')
