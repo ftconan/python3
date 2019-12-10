68ba6437e8fd49c89aebd2f8e871f451787a89ba
ftconan <1508907793@qq.com>
2019-12-10 11:49:21 +0800

    add python_version.sh

---
+++ effective_python/pythonic_thought/python_version.sh
@@ -0,0 +1,12 @@
+# @author: magician
+# @date: 2019/12/10
+# @file: virtual.sh
+
+python --version
+python3 --version
+
+# check version(python)
+# import sys
+
+# print(sys.version_info)
+# print(sys.version)
