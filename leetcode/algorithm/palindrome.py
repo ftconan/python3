8a2a0fb746f11e684002dd83853c1eaa46005797
ftconan <1508907793@qq.com>
2019-12-18 17:17:36 +0800

    add palindrome

---
+++ leetcode/algorithm/palindrome.py
@@ -0,0 +1,19 @@
+"""
+    @author: magician
+    @date: 2019/12/18
+    @file: palindrome.py
+"""
+import sys
+
+
+def is_palindrome(x: int) -> bool:
+    """
+    is_palindrome
+    :param x:
+    :return:
+    """
+    return bool(str(x) == str(x)[::-1])
+
+
+if __name__ == '__main__':
+    assert is_palindrome(121) is True
