30c575091cd958e0602609521d7fe103cee3c858
ftconan <1508907793@qq.com>
2019-12-19 14:57:21 +0800

    update valid_parentheses

--- leetcode/algorithm/valid_parentheses.py
+++ leetcode/algorithm/valid_parentheses.py
@@ -6,27 +6,23 @@
 
 
 def is_valid(s: str) -> bool:
-    flag = False
+    """
+    is_valid
+    :param s:
+    :return:
+    """
+    while '()' in s or '[]' in s or '{}' in s:
+        s = s.replace('()', '')
+        s = s.replace('[]', '')
+        s = s.replace('{}', '')
 
-    if len(s) % 2 == 0:
-        if len(s) == 2:
-            if s == '()' or s == '[]' or s == '{}':
-                flag = True
-        else:
-            s1 = s[:int(len(s) / 2):].replace('()', '').replace('[]', '').replace('{}', '')
-            s2 = s[int(len(s) / 2):].replace('()', '').replace('[]', '').replace('{}', '')
-            s3 = s2.replace(')', '(').replace(']', '[').replace('}', '{')[::-1]
-            print(s1, s2, s3)
-            if s1 == s3 and not (')(' in s2 or '][' in s2 or '}{' in s2):
-                flag = True
-
-    return flag
+    return s == ''
 
 
 if __name__ == '__main__':
-    # assert is_valid("()") is True
-    # assert is_valid("([])") is True
-    # assert is_valid("()[]{}") is True
-    # assert is_valid("((") is False
-    # assert is_valid("(()(") is False
+    assert is_valid("()") is True
+    assert is_valid("([])") is True
+    assert is_valid("()[]{}") is True
+    assert is_valid("((") is False
+    assert is_valid("(()(") is False
     assert is_valid("[({(())}[()])]") is True
