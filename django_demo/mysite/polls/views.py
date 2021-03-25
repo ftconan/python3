046d8fc873de5696ba7fb55d327d329615e3b3cb
ftconan <1508907793@qq.com>
2021-03-25 14:25:56 +0800

    add django app part3

--- django_demo/mysite/polls/views.py
+++ django_demo/mysite/polls/views.py
@@ -1,4 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question
 
 
 def index(request):
@@ -7,4 +10,47 @@ def index(request):
     @param request:
     @return:
     """
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+
+    return render(request, "polls/index.html", context)
+
+
+def detail(request, question_id):
+    """
+    detail
+    @param request:
+    @param question_id:
+    @return:
+    """
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    """
+    results
+    @param request:
+    @param question_id:
+    @return:
+    """
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    """
+    vote
+    @param request:
+    @param question_id:
+    @return:
+    """
+    return HttpResponse("You're voting on question %s." % question_id)
